e11e02e86752dd0d313913a2c90bd28c9ee00d5d
masakotpi <masakotpi@gmail.com>
2025-04-06 18:57:32 +0900

    テst

--- resources/views/recipe_mst_ingredient.blade.php
+++ resources/views/recipe_mst_ingredient.blade.php
@@ -8,14 +8,14 @@
 
 @section('content')
   @foreach(App\Consts\RecipeConst::INGREDIENT_TYPE as $index => $ingredient)
-  {{Form::open(['method' => 'get','id' => 'getindex'.$index, 'class'=>"d-inline"])}}
-    {{Form::hidden('type',$index,['form'=>'getindex'.$index])}}
+  <form method="get" id="getindex{{ $index }}" class="d-inline">
+    <input type="hidden" name="type" value="{{ $index }}" form="getindex{{ $index }}">
     @if($type == $index)
-    <button formtype="submit" formaction="{{route('mst_ingredients_index')}}" class="button btn-success btn-sm">{{$ingredient}}</button>
+    <button formtype="submit" formaction="/mst_ingredients" class="button btn-success btn-sm">{{$ingredient}}</button>
     @else
-    <button formtype="submit" formaction="{{route('mst_ingredients_index')}}" class="button btn-secondary btn-sm">{{$ingredient}}</button>
+    <button formtype="submit" formaction="/mst_ingredients" class="button btn-secondary btn-sm">{{$ingredient}}</button>
     @endif
-  {{Form::close()}}
+  </form>
 @endforeach
 
 <table class="table">
@@ -27,34 +27,44 @@
       <td widtd="15%">費用</td>
     </tr>
     <tr>
-      {{Form::open(['method' => 'post'])}}
-    <td widtd="40%">{{Form::text('name','',['class' =>'form-control'])}}</td>
-
-      {{Form::hidden('type',$type,['class' =>'form-control'])}}
-      <td>{{Form::text('unit','',['class' =>'form-control'])}}</td>
-      <td>{{Form::text('amount','',['class' =>'form-control'])}}</td>
-      <td>{{Form::text('cost','',['class' =>'form-control'])}}</td>
-      <td></td>
+      <form method="post">
+        <table>
+            <tr>
+                <td width="40%"><input type="text" name="name" class="form-control"></td>
+                <td><input type="hidden" name="type" value="{{ $type }}" class="form-control"></td>
+                <td><input type="text" name="unit" class="form-control"></td>
+                <td><input type="text" name="amount" class="form-control"></td>
+                <td><input type="text" name="cost" class="form-control"></td>
+                <td></td>
+            </tr>
+        </table>
+    </form>
+    
        {{-- 更新 --}}
     <td><button type="submit" formaction="{{route('mst_ingredients_store')}}" class="button btn-primary btn-sm">登録</button></td>
-    {{Form::close()}}
+    </form>
     </tr>
   </thead>
   @foreach($ingredients as $index => $ingredient)
   <tr class="my-0">
-    <td widtd="40%">{{Form::text('name',$ingredient->name,['class' =>'form-control', 'form'=>'form'.$index])}}</td>
-    {{Form::hidden('id',$ingredient->id,['form'=>'form'.$index])}}
-    <td>{{Form::text('unit',$ingredient->unit,['class' =>'form-control', 'form'=>'form'.$index])}}</td>
-    <td>{{Form::text('amount',$ingredient->amount,['class' =>'form-control', 'form'=>'form'.$index])}}</td>
-    <td>{{Form::text('cost',$ingredient->cost,['class' =>'form-control', 'form'=>'form'.$index])}}</td>
+  <td width="40%"><input type="text" name="name" value="{{ $ingredient->name }}" class="form-control" form="form{{ $index }}"></td>
+  <td><input type="hidden" name="id" value="{{ $ingredient->id }}" form="form{{ $index }}"></td>
+  <td><input type="text" name="unit" value="{{ $ingredient->unit }}" class="form-control" form="form{{ $index }}"></td>
+  <td><input type="text" name="amount" value="{{ $ingredient->amount }}" class="form-control" form="form{{ $index }}"></td>
+  <td><input type="text" name="cost" value="{{ $ingredient->cost }}" class="form-control" form="form{{ $index }}"></td>
+
     {{-- 削除 --}}
-    {{Form::open(['method' => 'delete', 'id' =>'delete'.$index])}}
+    <form method="post" id="delete{{ $index }}">
+      <input type="hidden" name="_method" value="DELETE">
+      <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <td><button formtype="submit" formaction="{{route('mst_ingredients_delete',['id' => $ingredient->id,'type'=>$type])}}" class="button btn-danger btn-sm">削除</button></td>
-    {{Form::close()}}
+    </form>
     {{-- 更新 --}}
-    {{Form::open(['method' => 'put', 'id' =>'form'.$index])}}
+    <form method="post" id="form{{ $index }}">
+      <input type="hidden" name="_method" value="PUT">
+      <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <td><button type="submit" formaction="{{route('mst_ingredients_update',['id' => $ingredient->id])}}" class="button btn-primary btn-sm">更新</button></td>
-    {{Form::close()}}
+    </form>
   </tr>
   @endforeach
 </table>
