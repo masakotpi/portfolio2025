25952226f635a92307c09ce403a8f088ad7f9ab5
masakotpi <masakotpi@gmail.com>
2025-04-06 15:47:39 +0900

    chn

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\RecipeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/nikoniko', function () {
+  return response()->json([
+      'name' => 'John Doe',
+      'email' => 'john@example.com'
+  ]);
 });
+
+
+ logger("APIルートにきたよ");
+  Route::get('/user', function (Request $request) {
+      return $request->user();
+  });
+
+
   //画像アップロード
   Route::post('/recipes/image', [RecipeController::class, 'upload'])->name('image_upload');
-
