279b914d57b1f648bcf965339c3270c76abbd60e
masakotpi <masakotpi@gmail.com>
2025-04-06 17:32:44 +0900

    assetes

--- resources/views/recipe_list.blade.php
+++ resources/views/recipe_list.blade.php
@@ -140,7 +140,8 @@ $color  = [
 @endsection
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="{{ asset('js/recipes.js') }}"></script>
+<script src="<?php echo asset('js/recipes.js'); ?>"></script>
+{{-- <script src="{{ asset('js/recipes.js') }}"></script> --}}
 
 <script>
   $(document).ready(function () {
