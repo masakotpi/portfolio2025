f008b41b9687d21bc87b0f24786d484419caccc7
masakotpi <masakotpi@gmail.com>
2025-04-10 20:22:43 +0900

    delete not required

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-   // .sass('resources/sass/app.scss', 'public/css')
    .version();
